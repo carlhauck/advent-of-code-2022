5cc4ab1907de82ff387f2865e3168205a2dd9f10
Carl Hauck <carlhauck@gmail.com>
2022-12-01 23:51:57 -0600

    adds day 2 solutions

---
+++ day-2-rock-paper-scissors.rb
@@ -0,0 +1,39 @@
+TOOL_USE_POINTS = {
+  X: 1, # rock
+  Y: 2, # paper
+  Z: 3, # scissors
+}
+
+OUTCOMES = {
+  X: { # rock
+    A: 3, # rock => draw
+    B: 0, # paper => loss
+    C: 6, # scissors => win
+  },
+  Y: { # paper
+    A: 6, # rock => win
+    B: 3, # paper => draw
+    C: 0, # scissors => loss
+  },
+  Z: { # scissors
+    A: 0, # rock => loss
+    B: 6, # paper => win
+    C: 3, # scissors => draw
+  }
+}
+
+def rock_paper_scissors(puzzle_input)
+  total = 0
+
+  puzzle_input.each do |line|
+    tools = line.split(" ")
+    opponent_tool = tools[0].to_sym
+    our_tool = tools[1].to_sym
+    total += TOOL_USE_POINTS[our_tool]
+    total += OUTCOMES[our_tool][opponent_tool]
+  end
+  p total
+end
+
+puzzle_input = File.open("puzzle_inputs/day-2.txt").readlines(chomp: true)
+rock_paper_scissors(puzzle_input)
---
+++ day-2-rock-paper-scissors2.rb
@@ -0,0 +1,32 @@
+OUTCOMES = {
+  A: { # rock
+    X: 3, # loss w/ scissors (0 + 3)
+    Y: 4, # draw w/ rock (3 + 1)
+    Z: 8, # win w/ paper (6 + 2)
+  },
+  B: { # paper
+    X: 1, # loss w/ rock (0 + 1)
+    Y: 5, # draw w/ paper (3 + 2)
+    Z: 9, # win w/ scissors (6 + 3)
+  },
+  C: { # scissors
+    X: 2, # loss w/ paper (0 + 2)
+    Y: 6, # draw w/ scissors (3 + 3)
+    Z: 7, # win w/ rock (6 + 1)
+  }
+}
+
+def rock_paper_scissors(puzzle_input)
+  total = 0
+
+  puzzle_input.each do |line|
+    letters = line.split(" ")
+    opponent_tool = letters[0].to_sym
+    needed_outcome = letters[1].to_sym
+    total += OUTCOMES[opponent_tool][needed_outcome]
+  end
+  p total
+end
+
+puzzle_input = File.open("puzzle_inputs/day-2.txt").readlines(chomp: true)
+rock_paper_scissors(puzzle_input)
